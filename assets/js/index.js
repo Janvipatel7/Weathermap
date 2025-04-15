d5e97dfbb329d3ebe7cc6d5da9336e70a22f21e7
janvipatel7 <janvi72004@gmail.com>
2025-04-15 16:52:09 +0530

    design added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,30 @@
-let inputCity = document.querySelector("#inputCity");
 let check = document.querySelector("#check");
-let ans = document.getElementById("weather-ans");
+let temp = document.getElementById("temp");
+let icon = document.getElementById("icon");
+let weatherDesc = document.getElementById("weatherDesc");
+let loc = document.getElementById("location");
+let feelsLike = document.getElementById("feelsLike");
+let humidity = document.getElementById("humidity");
+
 
 check.addEventListener("click" , ()=> {
-    let key = "18f47ff275a726c1f459b0d5f747ef0b"
-    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity.value}&units=metric&appid=${key}`
+    let cityName = document.querySelector("#cityName").value;
+    let cityKey = "18f47ff275a726c1f459b0d5f747ef0b"
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${cityKey}`
 
     fetch(URL)
         .then((res) => {
             return res.json()
         }).then((res) => {
-            ans.innerHTML =  `${res.main.temp } <img src="https://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png" alt="">`;
+            console.log(res);
+            temp.innerHTML = `${res.main.temp}`;
+            icon.src = `https://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`;
+            console.log(icon);
+            weatherDesc.innerHTML = res.weather[0].description;
+            loc.innerHTML = res.name;
+            feelsLike.innerHTML = `${res.main.feels_like}<h5 id="feelsLike"> Feels Like </h5>`;
+            humidity.innerHTML = res.main.humidity;
+            console.log(res);
+            
         })
 })
