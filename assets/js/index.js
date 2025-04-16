3274e64d8285e18a4eb4cc6aec90362a542aa4ef
janvipatel7 <janvi72004@gmail.com>
2025-04-16 16:07:30 +0530

    all validation done!!!!

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,17 +5,35 @@ let weatherDesc = document.getElementById("weatherDesc");
 let loc = document.getElementById("location");
 let feelsLike = document.getElementById("feelsLike");
 let humidity = document.getElementById("humidity");
+let searchCard = document.getElementById("searchCard");
+let weatherCard = document.getElementById("weatherCard");
+let backbtn = document.getElementById("backbtn");
+let cityName = document.querySelector("#cityName");
 
-
-check.addEventListener("click" , ()=> {
+check.addEventListener("click", () => {
     let cityName = document.querySelector("#cityName").value;
     let cityKey = "18f47ff275a726c1f459b0d5f747ef0b"
-    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${cityKey}`
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${cityKey}`;
+
+  
 
     fetch(URL)
         .then((res) => {
             return res.json()
         }).then((res) => {
+            if ((res.cod !== 200)) {
+                Swal.fire({
+                    icon: "warning",
+                    text: "State Not Found",
+                    position: 'bottom-start',
+                    toast: 'true',
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
+                document.querySelector("#cityName").value = "";
+                return;
+            }
+
             console.log(res);
             temp.innerHTML = `${res.main.temp}&#8451;`;
             icon.src = `https://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`;
@@ -25,6 +43,15 @@ check.addEventListener("click" , ()=> {
             feelsLike.innerHTML = `${res.main.feels_like}&#8451;`;
             humidity.innerHTML = `${res.main.humidity}%`;
             console.log(res);
-            
+
+            searchCard.classList.add("d-none");
+            weatherCard.classList.remove("d-none");
         })
 })
+
+backbtn.addEventListener("click", () => {
+    searchCard.classList.remove("d-none");
+    weatherCard.classList.add("d-none");
+    cityName.value = "";
+})
+
